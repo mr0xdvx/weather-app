7b463265b18df699652986d0343dfd490b734d25
Mr. 0xdvx <166197984+mr0xdvx@users.noreply.github.com>
2026-05-04 06:04:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+async function getWeather() {
+  let city = document.getElementById("city").value;
+
+  let apiKey = "YOUR_API_KEY";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  let res = await fetch(url);
+  let data = await res.json();
+
+  document.getElementById("result").innerHTML =
+    `Temperature: ${data.main.temp}°C <br>
+     Weather: ${data.weather[0].description}`;
+}
