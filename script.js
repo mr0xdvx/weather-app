aee2bf6e538bdf757c5d6ef53a107ee34b580c86
Mr. 0xdvx <166197984+mr0xdvx@users.noreply.github.com>
2026-05-04 06:26:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ async function getWeather() {
   let apiKey = "d40a76930272502ab1a671489f6aefdb";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  let res = await fetch(url);
-  let data = await res.json();
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
 
-  document.getElementById("result").innerHTML =
-    `Temperature: ${data.main.temp}°C <br>
-     Weather: ${data.weather[0].description}`;
+    // error handle
+    if (data.cod != 200) {
+      document.getElementById("result").innerHTML = "City not found ❌";
+      return;
+    }
+
+    document.getElementById("result").innerHTML =
+      `🌡 Temperature: ${data.main.temp}°C <br>
+       ☁ Weather: ${data.weather[0].description}`;
+
+  } catch (error) {
+    document.getElementById("result").innerHTML = "Error fetching data ⚠️";
+  }
 }
